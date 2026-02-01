0a30c758c6607ac801c5b201cb79797d61dc1f59
programmermosharof <mosharof.dev@gmail.com>
2026-02-01 21:33:08 +0600

    task2 complete

---
+++ task2.js
@@ -0,0 +1,24 @@
+function oddEven(numbers){
+    if(numbers % 2 ===0){
+        return numbers /2;
+    }
+    else{
+         return numbers * 2;
+    }
+    
+    }
+
+let result = oddEven(9);
+
+console.log(result);
+
+function evenNumber(numbers){
+if(numbers % 2 ===0){
+    return numbers / 2;
+}else{
+    return numbers * 2;
+}
+
+}
+const all = evenNumber(3);
+console.log(all);
