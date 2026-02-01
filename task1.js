4baf13298d8a8ab2670f4da74f024b943921e514
programmermosharof <mosharof.dev@gmail.com>
2026-02-01 21:04:29 +0600

    task 1 is complete

---
+++ task1.js
@@ -0,0 +1,20 @@
+// Take four parameters. Multiply the four numbers and then return the result
+
+ function multiply(a, b, c, d){
+    const result = a* b* c* d;
+
+    return result;
+ }
+
+const result = multiply(1, 5, 6, 7);
+console.log(result);
+ 
+// node task1.js
+
+function milt(f, s, t){
+   const sub = f- s-t;
+   return sub;
+}
+
+const sub = milt(70, 33, 5)
+console.log(sub);
